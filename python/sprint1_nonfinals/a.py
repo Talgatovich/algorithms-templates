57da1e7103cde873345788413cde8840db10c007
Раиль Ибятов <rtibyatov@gmail.com>
2022-03-30 17:12:32 +0300

    1

--- python/sprint1_nonfinals/a.py
+++ python/sprint1_nonfinals/a.py
@@ -1,6 +1,19 @@
-def evaluate_function(a: int, b: int, c: int, x: int) -> int:
-    # Здесь реализация вашего решения
-    pass
+def evaluate_function(a: int, b: int, c: int, x: int) -> int:    
+    res = (a * x ** 2) + (b * x) + c
+    return res
+
 
 a, x, b, c = map(int, input().strip().split())
 print(evaluate_function(a, b, c, x))
+
+
+# Здесь реализация вашего решения
+    #d = b ** 2 - 4 * a * c
+    #if d > 0:
+    #    x1 = (-b + sqrt(d)) / 2 * a
+    #    x2 = (-b - sqrt(d)) / 2 * a
+    #    return  x1, x2
+    #if d == 0:
+    #    x = -b / (2 * a)
+    #    return  x
+    #return None
