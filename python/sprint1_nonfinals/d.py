8aab702fde3ea1f63ef3564bc0566c99aa51b06c
Раиль Ибятов <rtibyatov@gmail.com>
2022-03-31 21:23:25 +0300

    weather

--- python/sprint1_nonfinals/d.py
+++ python/sprint1_nonfinals/d.py
@@ -1,9 +1,20 @@
 from typing import List
 
-def get_weather_randomness(temperatures: List[int]) -> int:
-    # Здесь реализация вашего решения
-    pass
 
+def get_weather_randomness(temperatures: List[int]) -> int:
+    count = 0
+    n = len(temperatures)-1
+    if len(temperatures) == 1:
+        return 1
+    if  temperatures[0] > temperatures[1]:
+            count += 1
+    if temperatures[n] > temperatures[n - 1]:
+            count += 1
+    for i in range(1, len(temperatures) - 1):
+        if temperatures[i - 1] < temperatures[i] > temperatures[i + 1]:
+            count += 1    
+    return count
+    
 def read_input() -> List[int]:
     n = int(input())
     temperatures = list(map(int, input().strip().split()))
