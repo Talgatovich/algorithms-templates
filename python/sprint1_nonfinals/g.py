8dd2a414f10a427b44208b2dec8aa0619df4a277
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-01 14:39:30 +0300

    binary'

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,6 +1,10 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    res = ''
+    while number != 0:
+        ans = number % 2
+        res += str(ans)
+        number = number // 2
+    return res[::-1]
 
 def read_input() -> int:
     return int(input().strip())
--- python/sprint1_nonfinals/h.py
+++ python/sprint1_nonfinals/h.py
@@ -1,8 +1,32 @@
 from typing import Tuple
 
 def get_sum(first_number: str, second_number: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+
+    first_number = first_number[::-1]
+    second_number = second_number[::-1]
+
+    size = max(len(first_number), len(second_number))
+
+    first_number += '0' * (size - len(first_number))
+    second_number += '0' * (size - len(second_number))
+
+    
+    overflow = 0
+    res = []
+    for obj in zip(first_number, second_number):
+        value = int(obj[0]) + int(obj[1]) + overflow
+        overflow = value // 2
+        res.append(value % 2)
+
+    
+    if overflow == 1:
+        res.append(1)
+    
+    res = res[::-1]
+    result = "".join(map(str, res))
+    
+    return result
+
 
 def read_input() -> Tuple[str, str]:
     first_number = input().strip()
