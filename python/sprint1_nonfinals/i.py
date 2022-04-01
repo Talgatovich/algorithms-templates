03bc21ad48c9d8ec38aa75d63bce80843dd64d4c
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-01 15:24:06 +0300

    power of four

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,10 @@
 def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    while number >= 4:
+        number = number / 4
+    if number == 1:
+        return True
+    return False
+    
+    
 
 print(is_power_of_four(int(input())))
