9bc7624081399d60e50ba50cb004ef4173eb45fe
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-01 20:20:14 +0300

    factorize

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -1,19 +1,20 @@
 from typing import List
 
+
 def factorize(n: int) -> List[int]:
-    
-    list = []
-    m = 0
-    while m < 2:        
-        dev = 2
-        for i in range(2, n//2):
-            if n % dev == 0:
-                list.append(dev)
-                m += 1
-                n = n // dev
-                dev = 2
-            else:
-                dev += 1
+    f = n
+    list = []            
+    dev = 2     
+    for i in range(1, n//2 + 1):
+        if f % dev == 0:
+            list.append(dev)            
+            f = f // dev
+            dev = 2
+        else:
+            dev += 1
+    if dev == n // 2 + 2:
+        list.append(n)
+        return list     
     return list
 
 
