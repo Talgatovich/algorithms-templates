149a72e9039a6231ce0e9f9d90de63fadc9a23f5
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-01 16:00:19 +0300

    factorize

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -1,8 +1,21 @@
 from typing import List
 
-def factorize(number: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+def factorize(n: int) -> List[int]:
+    
+    list = []
+    while m < 2:
+        m = 0
+        dev = 2
+        for i in range(2, n):
+            if n % dev == 0:
+                list.append(dev)
+                m += 1
+                n = n // dev
+                dev = 2
+            else:
+                dev += 1
+    return list
 
-result = factorize(int(input()))
+
+result = factorize(int(input("Enter n = ")))
 print(" ".join(map(str, result)))
