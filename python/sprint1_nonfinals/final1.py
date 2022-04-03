e18141e05ba6885fe482919540f7544b9f0e25b6
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-03 21:48:16 +0300

    finaltask1done

--- python/sprint1_nonfinals/final1.py
+++ python/sprint1_nonfinals/final1.py
@@ -7,32 +7,44 @@ def check_input(n):
     else:
         raise ValueError
 
-def check_null(numbers):
+def check_null2(numbers):
     null_index = []
-    for i in range(len(numbers)):
-        if numbers[i] == 0:
-            null_index.append(i)
+    while 0 in numbers:
+        current_index = numbers.index(0)
+        null_index.append(current_index)
+        numbers[current_index] = 1
     return null_index
+
+
 
 def get_distance(numbers, n):
     result = []
-    null_index = check_null(numbers)    
-    for i in range(n):
-        l = []
-        for j in range(len(null_index)):
-            a = abs(null_index[j] - i)
-            l.append(a)
-        b = min(l)
-        result.append(b)
+    null_index = None
+    for index, val in enumerate(numbers):
+        if val == 0:
+            null_index = index
+            result.append(0)
+            continue
+        if null_index != None:
+            result.append(index - null_index)
+        else:
+            result.append(10 ** 6 + index)
+    null_index = None
+    for index, val in reversed(list(enumerate(numbers))):
+        if val == 0:
+            null_index = index
+            continue
+        if null_index != None and result[index] > (null_index - index):
+            result[index] = (null_index - index)
     return result
 
 
 
 def read_input() -> Tuple[List[int], int, int]:
     n = int(input("input street length  "))
-    numbers = list(map(int, input("house's numbers ").strip().split())) 
-    n = check_input(n)   
+    numbers = list(map(int, input("house's numbers ").strip().split()))
+    n = check_input(n)
     return numbers, n
 
 n, numbers = read_input()
-print(" ".join(map(str,get_distance(n, numbers))))
+print(" ".join(map(str, get_distance(n, numbers))))
