41b58b70f6c4b3badb80579ed6367f3f21410007
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-06 13:10:33 +0300

    ss

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -4,18 +4,19 @@ from typing import List
 # Работает правильно, но долго. Необходимо исправить
 def factorize(n: int) -> List[int]:
     f = n
-    list = []            
-    dev = 2     
-    for i in range(1, n//2 + 1):
+    koef = n // 2
+    list = []
+    dev = 2
+    for i in range(1, koef + 1):
         if f % dev == 0:
-            list.append(dev)            
+            list.append(dev)
             f = f // dev
             dev = 2
         else:
             dev += 1
-    if dev == n // 2 + 2:
+    if dev == koef + 2:
         list.append(n)
-        return list     
+        return list
     return list
 
 
--- python/sprint1_nonfinals/k.py
+++ python/sprint1_nonfinals/k.py
@@ -1,14 +1,27 @@
 from typing import List, Tuple
 
+
 def get_sum(number_list: List[int], k: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    variable = "".join(map(str, number_list))
+    variable = str(int(variable) + k)
+    result = list(variable)
+    return result
+
 
 def read_input() -> Tuple[List[int], int]:
     n = int(input())
-    number_list = list(map(int, input().strip().split()))
+    number_list = list(input().strip().split())
     k = int(input())
     return number_list, k
 
+
 number_list, k = read_input()
-print(" ".join(map(str, get_sum(number_list, k))))
+print(" ".join(get_sum(number_list, k)))
+
+
+"""
+На вход подается количество цифр числа Х, списочная форма неотрицательного числа Х 
+и неотрицательное число K. Числа К и Х не превосходят 10000.
+
+Нужно вернуть списочную форму числа X + K.
+"""
