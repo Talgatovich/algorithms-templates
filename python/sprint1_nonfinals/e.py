a5a8c63b6337c78a2c410d51749229cd31e3eabe
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-12 09:22:12 +0300

    s12

--- python/sprint1_nonfinals/e.py
+++ python/sprint1_nonfinals/e.py
@@ -1,18 +1,21 @@
 def get_longest_word(line: str) -> str:
-    l = line.split(' ')
-    result = ''
+    l = line.split()
+    result = ""
     for word in l:
         if len(result) < len(word):
-            result = word        
+            result = word
     return result
+
 
 def read_input() -> str:
     _ = input()
     line = input().strip()
     return line
 
+
 def print_result(result: str) -> None:
     print(result)
     print(len(result))
+
 
 print_result(get_longest_word(read_input()))
