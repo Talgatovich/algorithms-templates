7656274e7a1ecf6ae4a2ffa0d34c4184e322f01b
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-15 18:41:48 +0300

    final

--- python/sprint1_nonfinals/temp.py
+++ python/sprint1_nonfinals/temp.py
@@ -1,17 +1,29 @@
-class Node:
-    def __init__(self, value=None, next=None):
-        self.value = value
-        self.next = next
+mylist = [0, 9, 1, 6, 0, 7, 3, 5, 2, 0, 0, 8, 4, 1, 0]
+n = len(mylist)
 
 
-def print_linked_list(vertex):
-    while vertex:
-        print(vertex.value, end=" -> ")
-        vertex = vertex.next
-    print("None")
+def nearest_zero(n):
+    # Подготваливаем массив для сбора информации о дистанциях до пустых участков
+    distances = [0] * n
+    index_dist = [i for i in range(n) if mylist[i] == 0]
+    # Находим индекс левого пустого участка
+    for i in range(distances[0], n, +1):
+        if mylist[i] == 0:
+            distances[i] = 0
+        else:
+            distances[i] = distances[i - 1] + 1
+    # Находим индекс правого пустого участка
+    for i in range(index_dist[-1], index_dist[0], -1):
+        if mylist[i] == 0:
+            distances[i] = 0
+        else:
+            distances[i] = min(distances[i], distances[i + 1] + 1)
+    # Заполняем информацию о дистанциях до пустых участков
+    for i in range(index_dist[0] - 1, -1, -1):
+        distances[i] = distances[i + 1] + 1
+    # Выводим информацию в нужном виде
+    return distances
 
 
-n3 = Node("third")
-n2 = Node("second", n3)
-n1 = Node("first", n2)
-print_linked_list(n1)
+distances = nearest_zero(n)
+print(" ".join(map(str, distances)))
---
+++ python/sprint_12/Final/deque.py
@@ -0,0 +1,135 @@
+# В первой строке записано количество команд n — целое число,
+# не превосходящее 100000. Во второй строке записано число m — максимальный
+# размер дека. Он не превосходит 50000.
+# В следующих n строках записана одна из команд:
+#
+# push_back(value) – добавить элемент в конец дека.
+# Если в деке уже находится максимальное число элементов, вывести «error».
+
+# push_front(value) – добавить элемент в начало дека.
+# Если в деке уже находится максимальное число элементов, вывести «error».
+
+# pop_front() – вывести первый элемент дека и удалить его.
+#  Если дек был пуст, то вывести «error».
+
+# pop_back() – вывести последний элемент дека и удалить его.
+#  Если дек был пуст, то вывести «error».
+
+# Value — целое число, по модулю не превосходящее 1000.
+# Формат вывода
+# Выведите результат выполнения каждой команды на отдельной строке.
+# Для успешных запросов push_back(x) и push_front(x) ничего выводить не надо.
+
+
+class Deque:
+    def __init__(self, m):
+        self.deq = [None] * m
+        self.maximum = m
+
+        self.front_head = 0  # front голова
+        self.front_tail = 0  # front хвост
+
+        self.back_tail = 1  # back хвост
+        self.back_head = 0  # back голова
+
+        self.current_size = 0
+
+    def is_empty(self):
+        return self.current_size == 0
+
+    def is_full(self):
+        return self.current_size >= self.maximum
+
+    def push_front(self, value):
+        if self.is_full():
+            print("error")
+            return
+        if self.current_size != self.maximum:
+            self.deq[self.front_head] = value
+            self.current_size += 1
+            self.front_head = (self.front_head - 1) % self.maximum
+            print("Текущий ДЕК --добавление в начало--", self.deq)  # DELETE
+            print("Обновленная front голова --- ", self.front_head)  # DELETE
+        return
+
+    def push_back(self, value):
+        if self.is_full():
+            print("error")
+            return
+        if self.current_size != self.maximum:
+            print("Текущий back хвост ---", self.back_tail)  # DELETE
+            self.deq[self.back_tail] = value
+            self.current_size += 1
+            self.back_tail = (self.back_tail + 1) % self.maximum
+            print("Текущий ДЕК --добавление в конец--", self.deq)  # DELETE
+            print("Обновлённый back хвост ---", self.back_tail)  # DELETE
+            print("Обновленная back голова ---", self.back_head)  # DELETE
+        return
+
+    def pop_front(self):
+        if self.is_empty():
+            print("error")
+            return
+        first_elem = self.deq[(self.front_head + 1) % self.maximum]
+        self.deq[self.front_head] = None
+        self.front_head = (self.front_head + 1) % self.maximum
+        self.current_size -= 1
+        print(first_elem, "ВЫВОД")
+        print("Текущий ДЕК --удаление из начала--", self.deq)  # DELETE
+        print("Текущая front голова --- ", self.front_head)  # DELETE
+        return
+
+    def pop_back(self):
+        if self.is_empty():
+            print("error")
+            return
+        last_elem = self.deq[(self.back_tail - 1) % self.maximum]
+        self.deq[self.back_tail - 1] = None
+        # if last_elem == None:
+        #    last_elem = self.deq[self.back_head]
+        #    self.deq[self.back_head] = None
+
+        self.back_tail = (self.back_tail - 1) % self.maximum
+        self.current_size -= 1
+        print(last_elem, "ВЫВОД")
+        print("Текущий ДЕК --удаление с конца--", self.deq)  # DELETE
+        print("Текущий back хвост ---", self.back_tail)  # DELETE
+        print("Обновленная back голова ---", self.back_head)  # DELETE
+        return
+
+
+def read_input():
+    n = int(input())  # Количество комманд
+    m = int(input())  # максимально допустимый размер очереди
+
+    queue = Deque(m)
+    for _ in range(n):
+        commands = list(input().strip().split(" "))
+        com = commands[0]
+        f = getattr(queue, com)
+        if len(commands) > 1:
+            val = commands[1]
+            f(val)
+        else:
+            f()
+
+
+def main():
+    read_input()
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+4
+4
+push_front 861
+push_front -819
+pop_back
+pop_back
+
+Вывод 
+20
+102
+"""
