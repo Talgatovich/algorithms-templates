148d102defbc87f8583e9f9dd4938e1cc05fefe7
Раиль Ибятов <rtibyatov@gmail.com>
2022-04-20 17:40:23 +0300

    codewars1

---
+++ python/codewar/uniq_in_order.py
@@ -0,0 +1,21 @@
+def unique_in_order(a):
+    result = []
+    for i in range(1, len(a) - 1):
+        if a[i - 1] != a[i] != a[i + 1]:
+            result.append(a[i])
+    print(result)
+    return result
+
+
+def testing():
+    a = "AAAABBBCCDAABBB"
+    b = ["A", "B", "C", "D", "A", "B"]
+    c = "ABBCcAD"
+    d = ["A", "B", "C", "c", "A", "D"]
+
+    print("РАБОТАЕТ" if unique_in_order(a) == b else "Что-то надо исправить")
+    print("РАБОТАЕТ" if unique_in_order(c) == d else "Что-то надо исправить")
+
+
+if __name__ == "__main__":
+    testing()
