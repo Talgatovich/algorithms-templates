80cbc38d8389de664a40df75c64f515db182ad0f
Раиль Ибятов <rtibyatov@gmail.com>
2022-05-04 21:55:47 +0300

    s13

--- python/sprint_13/broken_search.py
+++ python/sprint_13/broken_search.py
@@ -1,23 +1,25 @@
-ASD = []
-
-
-def binarySearch(arr, x, left, right):
-    if right <= left:
-        return -1
-    mid = (left + right) // 2
-    if arr[mid] == x:
-        ASD.append(mid)
-        return
-    elif x != arr[mid]:
-        binarySearch(arr, x, left, mid)
-        binarySearch(arr, x, mid + 1, right)
-
-
+# 68096897
 def broken_search(nums, target) -> int:
-    left = 0
-    right = len(nums)
-    binarySearch(nums, target, left, right)
-    return ASD[0] if len(ASD) != 0 else -1
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = start + (end-start) // 2
+        if nums[mid] == target:
+            return mid       
+        if nums[start] <= nums[mid]:
+            if target < nums[mid] and target >= nums[start]:
+                end = mid - 1
+            else:
+                start = mid + 1            
+        else:
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+            
+    return -1
+
+
 
 
 def test():
--- python/sprint_13/code.py
+++ python/sprint_13/code.py
@@ -13,27 +13,26 @@
 .
 Изменять массив нельзя.
 """
-
-ASD = []
-
-
-def binarySearch(arr, x, left, right):
-    if right <= left:
-        return -1
-    mid = (left + right) // 2
-    if arr[mid] == x:
-        ASD.append(mid)
-        return
-    elif x != arr[mid]:
-        binarySearch(arr, x, left, mid)
-        binarySearch(arr, x, mid + 1, right)
-
-
 def broken_search(nums, target) -> int:
-    left = 0
-    right = len(nums)
-    binarySearch(nums, target, left, right)
-    return ASD[0] if len(ASD) != 0 else -1
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = start + (end-start) // 2
+        if nums[mid] == target:
+            return mid       
+        if nums[start] <= nums[mid]:
+            if target < nums[mid] and target >= nums[start]:
+                end = mid - 1
+            else:
+                start = mid + 1            
+        else:
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+            
+    return -1
+
 
 
 def test():
@@ -41,6 +40,24 @@ def test():
     assert broken_search(arr, 5) == 6
 
 
-nums = [19, 21, 100, 101, 1, 4, 5, 7, 12, 57, 36, 20, 47, 21]
-target = 5
+nums = [5, 1]
+target = 1
 print(broken_search(nums, target))
+
+
+
+# =======================================================================
+def binary_search_for_broken(arr: list, search_value: int) -> int:
+    start_value  = arr[0]
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        middle = start + (end-start) // 2
+        if search_value == arr[middle]:
+            return middle
+        
+        if (arr[middle] < start_value) is not (search_value < start_value) or (arr[middle] > search_value):
+            end = middle - 1
+        else:
+            start = middle + 1
+    return -1  
