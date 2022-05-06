30a2592c94dfa0aaee8db4bad2782699754f8cb3
Раиль Ибятов <rtibyatov@gmail.com>
2022-05-06 09:14:40 +0300

    s13Final

---
+++ python/sprint_13/Final/broken_search.py
@@ -0,0 +1,25 @@
+# 68096897
+def broken_search(nums, target) -> int:
+    start = 0
+    end = len(nums) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[start] <= nums[mid]:
+            if target < nums[mid] and target >= nums[start]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+    return -1
+
+
+def test():
+    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+    assert broken_search(arr, 5) == 6
---
+++ python/sprint_13/Final/qsort.py
@@ -0,0 +1,51 @@
+# 68124998
+def partition(arr, left, right):
+    pivot = arr[left]
+    i = left + 1
+    j = right - 1
+    while True:
+        if i <= j and arr[j] > pivot:
+            j -= 1
+        elif i <= j and arr[i] < pivot:
+            i += 1
+        elif (arr[j] > pivot) or (arr[i] < pivot):
+            continue
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            arr[left], arr[j] = (
+                arr[j],
+                arr[left],
+            )
+            return j
+
+
+def quick_sort(arr, left, right):
+    if (right - left) > 1:
+        m = partition(arr, left, right)
+        quick_sort(arr, left, m)
+        quick_sort(arr, m + 1, right)
+
+
+def transform(arr):
+    arr[1] = -int(arr[1])
+    arr[2] = int(arr[2])
+
+    return [arr[1], arr[2], arr[0]]
+
+
+def read_input():
+    n = int(input())
+    arr = [transform(input().split()) for _ in range(n)]
+    return n, arr
+
+
+def main():
+    n, arr = read_input()
+    left = 0
+    quick_sort(arr, left, len(arr))
+    print(*(list(zip(*arr))[2]), sep="\n")
+
+
+if __name__ == "__main__":
+    main()
